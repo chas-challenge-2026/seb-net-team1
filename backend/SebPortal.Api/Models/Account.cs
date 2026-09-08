081467659a2f1f7d2197a4a9b052637cbf7aaf0e
Johan <johan.soderdahl@chasacademy.se>
2026-09-08 14:08:38 +0200

    Added some entities

---
+++ backend/SebPortal.Api/Models/Account.cs
@@ -0,0 +1,12 @@
+﻿public class Account
+{
+    public int Id { get; set; }
+    public string AccountNumber { get; set; }
+    public decimal Balance { get; set; }
+    public string? AccountType { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+    public int ISBN { get; set; }
+    public ICollection<Transaction> Transactions { get; set; }
+        = new List<Transaction>();
+}
---
+++ backend/SebPortal.Api/Models/Transaction.cs
@@ -0,0 +1,10 @@
+﻿public class Transaction
+{
+    public int Id { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string? Description { get; set; }
+    public string? TransactionType { get; set; }
+    public int AccountId { get; set; }
+    public Account? Account { get; set; }
+}
