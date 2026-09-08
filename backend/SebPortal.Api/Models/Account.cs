629fd66e2bda83ce9ef278b07447c21711fd44b4
Johan <johan.soderdahl@chasacademy.se>
2026-09-08 14:11:25 +0200

    Was a typo which is now removed

--- backend/SebPortal.Api/Models/Account.cs
+++ backend/SebPortal.Api/Models/Account.cs
@@ -5,7 +5,6 @@
     public decimal Balance { get; set; }
     public string? AccountType { get; set; }
     public int CustomerId { get; set; }
-    public Customer Customer { get; set; }
     public int ISBN { get; set; }
     public ICollection<Transaction> Transactions { get; set; }
         = new List<Transaction>();
