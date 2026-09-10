a31ed93af9e6487d494887670fb7d89f7a4ef8a2
Johan <johan.soderdahl@chasacademy.se>
2026-09-10 11:07:02 +0200

    Added more models based of ER-Diagram

--- backend/SebPortal.Api/Models/Account.cs
+++ backend/SebPortal.Api/Models/Account.cs
@@ -5,7 +5,7 @@
     public decimal Balance { get; set; }
     public string? AccountType { get; set; }
     public int CustomerId { get; set; }
-    public int ISBN { get; set; }
+    public int IBAN { get; set; }
     public ICollection<Transaction> Transactions { get; set; }
         = new List<Transaction>();
 }
