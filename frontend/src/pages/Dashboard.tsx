8a9a958b2a912919921f75de029b253cf868a716
nm <nm.codes@hotmail.com>
2026-09-10 20:30:15 +0200

    Add: Implement PendingApprovals component to display a list of pending approvals with associated payment and approver details.

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,6 +5,7 @@ import DashboardSummary from "../components/dashboard/DashboardSummary";
 import AccountOverview from "../components/dashboard/AccountOverview";
 import PaymentStatus from "../components/dashboard/PaymentStatus";
 import UpcomingPayments from "../components/dashboard/UpcomingPayments";
+import PendingApprovals from "../components/dashboard/PendingApprovals";
 import "../styles/dashboard.css";
 
 function Dashboard() {
@@ -39,6 +40,7 @@ function Dashboard() {
           <PaymentStatus />
         </section>
         <UpcomingPayments />
+        <PendingApprovals />
       </main>
     </div>
   );
