23678f822b8e25008c26883f5d49303eccb41986
Johan <johan.soderdahl@chasacademy.se>
2026-09-16 09:56:12 +0200

    removed spelling error

--- backend/SebPortal.Api/Services/PaymentService.cs
+++ backend/SebPortal.Api/Services/PaymentService.cs
@@ -40,7 +40,7 @@ public class PaymentService(PaymentRepository paymentRepository)
             Reference = reference,
             CreatedById = createdById,
             CreatedAt = DateTime.UtcNow,
-            Status = PaymentStatuses.PendingApproval //a
+            Status = PaymentStatuses.PendingApproval 
         };
 
         // 3. Checks if attest is needed ---------- right now you yourself says if its true or false
